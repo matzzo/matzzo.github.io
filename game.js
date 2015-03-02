4bcb811af4b9e2dc932fac7b19ba62bcc64d1797
Benjamin Snoha <benjamin@snoha.com>
2015-03-02 11:13:58 -0700

    Centered and added click detection

--- game.js
+++ game.js
@@ -1,24 +1,42 @@
 //Benjamin Snoha
 
 var c;
-var x = 100;
-var y = 100;
 var RECTWIDTH = 100;
 var RECTHEIGHT = 100;
+var PADDING = 10;
+var NUMBEROFTILES = 9;
+
+var mouseX;
+var mouseY; 
+var squares = new Array();
 
 //Init variables
 function init() {
 	//Set up canvas
 	c = document.getElementById('_c');
-	c.width = window.innerWidth;
-	c.height = window.innerHeight;
-    canvas = c.getContext('2d');
+	c.width = (RECTWIDTH * 3) + (PADDING * 2);
+	c.height = (RECTHEIGHT * 3) + (PADDING * 2);
+	canvas = c.getContext('2d');
 	
 	//Add mouse click listener
     c.addEventListener("click", onClick, false);
 
 	//Clear the screen.
     clear();
+
+	//Make the array of squares
+	for(var i = 0; i < NUMBEROFTILES; i++){
+		squares[i] = new Array();
+	}
+	
+	var counter = 0;
+	for(var x = 0; x < NUMBEROFTILES/3; x++){
+		for(var y = 0; y < NUMBEROFTILES/3; y++){
+			squares[counter][0] = x * (RECTWIDTH + PADDING);
+			squares[counter][1] = y * (RECTHEIGHT + PADDING);
+			counter++;
+		}
+	}
 	
 	//Set the interview for the loop.
     setInterval(loop, 15);
@@ -30,7 +48,10 @@ function loop() {
 	
 	//Draw a rect.
 	canvas.fillStyle="#aabbcc";
-    canvas.fillRect(x, y, RECTWIDTH, RECTHEIGHT);
+	
+	for(var i = 0; i < NUMBEROFTILES; i++){
+		canvas.fillRect(squares[i][0], squares[i][1], RECTWIDTH, RECTHEIGHT);
+	}
 }
 
 //Clear the screen.
@@ -43,26 +64,18 @@ function clear() {
 
 //When the user clicks. 
 function onClick(e) {
-    var element = canvas;
-    var offsetX = 0
-	var offsetY = 0
-
-	if (element.offsetParent){
-      do {
-        offsetX += element.offsetLeft;
-        offsetY += element.offsetTop;
-      } while ((element = element.offsetParent));
-    }
-
-	//The the x and y coordinates of the mouse click.
-    var tempx = e.pageX - offsetX;
-    var tempy = e.pageY - offsetY;
+	var rect = c.getBoundingClientRect();  // get element's abs. position
+    var tempx = e.clientX - rect.left;     // get mouse x and adjust for el.
+    var tempy = e.clientY - rect.top;      // get mouse y and adjust for el.
+	
 	
 	//Check that the click is within the square. 
-	if(tempx < x + RECTWIDTH && tempx > x){
-		if(tempy < y + RECTHEIGHT && tempy > y)
-		{
-			alert("Clicked Square");
+	for(var i = 0; i < NUMBEROFTILES; i++){
+		if(tempx < squares[i][0] + RECTWIDTH && tempx > squares[i][0]){
+			if(tempy < squares[i][1] + RECTHEIGHT && tempy > squares[i][1])
+			{
+				alert("Clicked Square: " + i);
+			}
 		}
 	}
 }
